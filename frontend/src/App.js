9c2823bdbf9b69c3e9538c7d75e98ba6248fb794
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-11-05 18:20:13 +0100

    Create routes, import list component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,22 @@ import './App.css';
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import LandingPage from './screens/LandingPage/LandingPage'
+import MyList from './screens/MyList/MyList'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="App">
+      <Router>
       <Header />
       <main>
-        <LandingPage />
+        <Routes>
+        <Route path='/' element={<LandingPage />} exact/>
+        <Route path='/mylist' element={<MyList />} />
+        </Routes>
       </main>
       <Footer />
+      </Router>
     </div>
   );
 }
