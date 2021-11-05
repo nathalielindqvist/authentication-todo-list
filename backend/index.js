9ce8f0b24ec1ba98c82ca4796ad6594e838b932f
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-11-05 13:18:52 +0100

    Start server setup

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const dotenv = require("dotenv");
+const express = require('express');
+const notes = require('./data/notes');
+
+const app = express();
+dotenv.config();
+
+app.get('/', (req, res) => {
+  res.send("API is running...");
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  res.send(note);
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, console.log(`Server is running on PORT ${PORT}`));
