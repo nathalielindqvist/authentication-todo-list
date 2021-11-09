cfc4804ae0a306dc4e1665bc1139a489847ffeae
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-11-09 21:46:41 +0100

    Frontend routes login + register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import LandingPage from './screens/LandingPage/LandingPage'
-import MyList from './screens/MyList/MyList'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import LandingPage from './screens/LandingPage/LandingPage';
+import LoginPage from './screens/LoginPage/LoginPage';
+import RegisterPage from './screens/RegisterPage/RegisterPage';
+import MyList from './screens/MyList/MyList';
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
       <main>
         <Routes>
         <Route path='/' element={<LandingPage />} exact/>
+        <Route path='/login' element={<LoginPage />} exact/>
+        <Route path='/register' element={<RegisterPage />} exact/>
         <Route path='/mylist' element={<MyList />} />
         </Routes>
       </main>
