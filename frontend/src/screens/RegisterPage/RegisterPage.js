06f92c4d65b01b70efc62bca25a7671b3cfef876
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-11-11 16:27:33 +0100

    Move logic to register action

--- frontend/src/screens/RegisterPage/RegisterPage.js
+++ frontend/src/screens/RegisterPage/RegisterPage.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Button, Col, Form, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { register } from '../../actions/userActions';
 import ErrorMessage from '../../components/ErrorMessage';
 import Loading from '../../components/Loader/Loading';
 import MainScreen from '../../components/MainScreen'
@@ -17,40 +18,30 @@ const RegisterPage = () => {
   const [confirmpassword, setConfirmPassword] = useState("");
   const [message, setMessage] = useState(null);
   const [picMessage, setPicMessage] = useState(null);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const userRegister = useSelector(state => state.userRegister);
+  const { loading, error, userInfo } = userRegister;
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate("/mylist");
+    }
+  }, [navigate, userInfo])
+
 
   const submitHandler = async (e) => {
     e.preventDefault();
 
     if(password !== confirmpassword) {
-      setMessage("Passwords do not match");
-    } else {
-      setMessage(null)
-      try {
-        const config = {
-          headers: {
-            "Content-type": "application/json",
-          },
-        };
-
-        setLoading(true);
-
-        const { data } = await axios.post(
-          "/api/users",
-          { name, pic, email, password },
-          config
-        );
-
-        setLoading(false);
-        localStorage.setItem("userInfo", JSON.stringify(data));
-        console.log(data);
-
-      } catch (err) {
-        setError(err.response.data.message);
-      }
+      setMessage('Passwords do not match');
     }
-
+    else {
+      dispatch(register(name, email, password, pic));
+    }
   }
 
   const postDetails = (pics) => {
