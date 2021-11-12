5fd1b499018449507ddb691cfb3cc6805eec93b9
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-11-12 14:56:34 +0100

    Task schema for mongodb

---
+++ backend/models/taskModel.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const mongoose = require('mongoose');
+
+const taskSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
