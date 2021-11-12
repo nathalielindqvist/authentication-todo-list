204a2d3d3ff62d405315806165633c6cc5971c0c
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-11-12 17:14:19 +0100

    Display user name + display login when logged out

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -31,10 +31,12 @@ const Header = () => {
       </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
+
+      {userInfo ?
       <Nav className="mr-auto">
         <Link className="nav-link" to="/mylist">My list</Link>
-        <NavDropdown title="User" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#">
+        <NavDropdown title={userInfo?.name} id="basic-nav-dropdown">
+          <NavDropdown.Item href="/profile">
             Profile
               </NavDropdown.Item>
           <NavDropdown.Divider />
@@ -44,6 +46,9 @@ const Header = () => {
             </NavDropdown.Item>
         </NavDropdown>
       </Nav>
+      : <Nav>
+        <Link className="nav-link" to="/login">Login</Link>
+        </Nav>}
     </Navbar.Collapse>
   </Container>
 </Navbar>
