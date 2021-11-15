a89c35ad7185d7431968e616bd69cdb172d847c2
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-11-15 20:46:46 +0100

    Move footer css to seperate file

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,16 +1,10 @@
 import { Container, Row, Col } from "react-bootstrap";
+import './Footer.css'
 
 const Footer = () => {
   return (
     <div>
-      <footer
-      style={{
-        width: "100%",
-        position: "relative",
-        bottom: 0,
-        display: "flex",
-        justifyContent: "center",
-      }}>
+      <footer className="footer">
 
         <Container>
           <Row>
